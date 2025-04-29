dcf5a65b4c9cfa0625987cc22f732770f5337e2f
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-04-29 22:37:33 +0530

    Initialized views which will be a functional viewing, reviewing and confirming the booking views soon

--- djangowebsite/booking/urls.py
+++ djangowebsite/booking/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 app_name="booking"
 urlpatterns = [
     path("",views.index,name="index"),
-    path("/booking",views.booking_slot,name="booking_slot")
+    path("book_slot/",views.booking_slot,name="booking_slot"),
+    path("<int:pk>/results/", views.BookDetailView.as_view(),name="book_review")
 ]
--- djangowebsite/booking/views.py
+++ djangowebsite/booking/views.py
@@ -4,7 +4,13 @@ from django.views import generic
 from .models import Book
 
 def index(request):
-    return render(request,"booking/index.html")
+    return render(request, "booking/index.html")
 
 def booking_slot(request):
-    return HttpResponse("Booking is done here")
+    return render(request, "booking/book_slot.html")
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name= "booking/book_review.html"
+
+
