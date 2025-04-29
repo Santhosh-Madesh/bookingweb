55a75c29449f9373fb094c0b8b75434c674921cc
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-04-30 02:58:21 +0530

    Created a test to see if date and hour repeats more than onc that is two objects have been made with same date and same houre

--- djangowebsite/booking/tests.py
+++ djangowebsite/booking/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Slot
+import datetime
 
+class SlotModelTest(TestCase):
+    def test_if_date_hour_repeats(self):
+        book1 = Slot(date=datetime.date.today(),hour=5,is_booked=True)
+        book2 = Slot(date=datetime.date.today(),hour=5,is_booked=True)
+
+        flag = False
+
+        if book1.date == book2.date and book1.hour == book2.hour:
+            flag = True
+
+            
+        self.assertIs(flag,False)
 
