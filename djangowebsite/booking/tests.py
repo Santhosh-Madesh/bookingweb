c6cb931367faed88529a6036d8fe5046b594e3a1
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-04-30 14:02:47 +0530

    Created a test that fails if booking is done in the past

--- djangowebsite/booking/tests.py
+++ djangowebsite/booking/tests.py
@@ -7,11 +7,21 @@ class SlotModelTest(TestCase):
         book1 = Slot(date=datetime.date.today(),hour=5,is_booked=True)
         book2 = Slot(date=datetime.date.today(),hour=5,is_booked=True)
 
-        flag = False
+        flag = True
 
         if book1.date == book2.date and book1.hour == book2.hour:
-            flag = True
+            flag = False
 
             
-        self.assertIs(flag,False)
+        self.assertIs(flag,True)
+    
+    def test_if_date_is_in_the_past(self):
+        book1 = Slot(date=datetime.date(2025,4,29),hour=4,is_booked=True)
+
+        flag = True
+
+        if book1.date<datetime.date.today():
+            flag = False
+
+        self.assertIs(flag,True)
 
